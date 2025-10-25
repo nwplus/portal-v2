abe65f2d2f5421c3b4589e9890d0fc21aee13712
martincai8 <martincai46@gmail.com>
2025-10-25 16:48:02 -0700

    cleanup

--- src/routes/$activeHackathon/_auth/application/basic-info.tsx
+++ src/routes/$activeHackathon/_auth/application/basic-info.tsx
@@ -1,5 +1,4 @@
 import { useHackathon } from "@/hooks/use-hackathon";
-import { useApplicantStore } from "@/lib/stores/applicant-store";
 import { useApplicationQuestionStore } from "@/lib/stores/application-question-store";
 import { Link, createFileRoute } from "@tanstack/react-router";
 
@@ -10,7 +9,6 @@ export const Route = createFileRoute("/$activeHackathon/_auth/application/basic-
 function RouteComponent() {
   const { activeHackathon } = useHackathon();
   const questions = useApplicationQuestionStore((s) => s.basicInfoQuestions);
-  const applicantDraft = useApplicantStore((s) => s.applicantDraft);
 
   if (!questions.length) {
     return <div>No basic info questions yet.</div>;
@@ -29,30 +27,6 @@ function RouteComponent() {
           </li>
         ))}
       </ul>
-
-      <div className="flex flex-col gap-4">
-        <div className="flex flex-col gap-2">
-          <p>Legal Last Name: {applicantDraft?.basicInfo?.legalLastName}</p>
-          <p>Legal First Name: {applicantDraft?.basicInfo?.legalFirstName}</p>
-          <p>Preferred Name: {applicantDraft?.basicInfo?.preferredName}</p>
-        </div>
-        <div className="flex flex-col gap-2">
-          <p>Phone Number: {applicantDraft?.basicInfo?.phoneNumber}</p>
-          <p>Email: {applicantDraft?.basicInfo?.email}</p>
-          <p>Gender: {JSON.stringify(applicantDraft?.basicInfo?.gender)}</p>
-          <p>Graduation: {applicantDraft?.basicInfo?.graduation}</p>
-        </div>
-      </div>
-      <div className="flex flex-col gap-2">
-        <p>Country of Residence: {applicantDraft?.basicInfo?.countryOfResidence}</p>
-        <p>Education Level: {applicantDraft?.basicInfo?.educationLevel}</p>
-        <p>Major: {JSON.stringify(applicantDraft?.basicInfo?.major)}</p>
-        <p>School: {applicantDraft?.basicInfo?.school}</p>
-      </div>
-      <div className="flex flex-col gap-2">
-        <p>Cultural Background: {JSON.stringify(applicantDraft?.basicInfo?.culturalBackground)}</p>
-        <p>Dietary Restriction: {JSON.stringify(applicantDraft?.basicInfo?.dietaryRestriction)}</p>
-      </div>
 
       <div className="flex gap-2 pt-2">
         <Link to="/$activeHackathon/application" params={{ activeHackathon }}>
