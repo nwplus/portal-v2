ef1a3d25e0c645543bb2fe63accd6ffcf2c66bc5
Kezia <roselinakeziarijadi@gmail.com>
2025-12-26 08:28:07 -0800

    Revert "linting"
    This reverts commit 1f4e6384999ecdac1e2cebef3cbb0d6ff813b8c1.

--- src/routes/$activeHackathon/_auth.tsx
+++ src/routes/$activeHackathon/_auth.tsx
@@ -1,12 +1,11 @@
 import { useAuthStore } from "@/lib/stores/auth-store";
 import { loadAuth } from "@/lib/utils";
-import { fetchApplicant } from "@/services/applicants";
 import { Outlet, createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/$activeHackathon/_auth")({
-  beforeLoad: async ({ location, params, context }) => {
+  beforeLoad: async ({ location, params }) => {
     await loadAuth();
-    const { isAuthenticated, user } = useAuthStore.getState();
+    const { isAuthenticated } = useAuthStore.getState();
 
     if (!isAuthenticated) {
       throw redirect({
@@ -18,22 +17,6 @@ export const Route = createFileRoute("/$activeHackathon/_auth")({
           redirect: location.pathname,
         },
       });
-    }
-
-    const isOnApplicationPage = location.pathname.includes("/application");
-    const { dbCollectionName } = context;
-
-    if (!isOnApplicationPage && user?.uid && dbCollectionName) {
-      const applicant = await fetchApplicant(dbCollectionName, user.uid);
-
-      if (!applicant || !applicant.submission?.submitted) {
-        throw redirect({
-          to: "/$activeHackathon/application",
-          params: {
-            activeHackathon: params.activeHackathon,
-          },
-        });
-      }
     }
   },
   component: () => <RouteComponent />,
