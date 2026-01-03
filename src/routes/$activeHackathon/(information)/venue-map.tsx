f50eb14ad3c23ea080fe70bad4ac3c5b6b2d2cdc
naijwu <jaewuchun@gmail.com>
2026-01-03 02:51:35 -0800

    add image viewer (large viewports only)

--- src/routes/$activeHackathon/(information)/venue-map.tsx
+++ src/routes/$activeHackathon/(information)/venue-map.tsx
@@ -1,5 +1,9 @@
+import { Container } from "@/components/layout/container";
 import { GradientBackground } from "@/components/layout/gradient-background";
 import { createFileRoute } from "@tanstack/react-router";
+
+import { ImageViewer } from "@/components/ui/image-viewer";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export const Route = createFileRoute("/$activeHackathon/(information)/venue-map")({
   component: RouteComponent,
@@ -8,7 +12,20 @@ export const Route = createFileRoute("/$activeHackathon/(information)/venue-map"
 function RouteComponent() {
   return (
     <GradientBackground gradientPosition="bottomMiddle">
-      Hello "/$activeHackathon/(information)/venue-map"!
+      <Container>
+        <Tabs defaultValue="main">
+          <TabsList className="relative">
+            <TabsTrigger value="main">Main venue</TabsTrigger>
+            <TabsTrigger value="ceremonies">Ceremonies venue</TabsTrigger>
+          </TabsList>
+          <TabsContent value="main">
+            <ImageViewer className="-mt-11" src="/assets/maps/lsi.png" />
+          </TabsContent>
+          <TabsContent value="ceremonies">
+            <ImageViewer className="-mt-11" src="/assets/maps/ceremonies.png" />
+          </TabsContent>
+        </Tabs>
+      </Container>
     </GradientBackground>
   );
 }
